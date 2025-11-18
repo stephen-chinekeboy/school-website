97961117c0baae3bdaca9b3163161d21dc6da66e
stephen-chinekeboy <idokostephen93@gmail.com>
2025-11-18 23:10:00 +0100

    still working on the registration page.

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function goToReg() {
-  window.location.href="/registration.html"
-
+  window.location.href="/registration.html";
 }
+
+// const paraElement = document.querySelector('.js-copy-button').classList('copy').textContent = new Date().getFullYear();
+
+
 
 
