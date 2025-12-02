dcdbb0b4b770a1b14ae5f3494f81a134c29040fc
stephen-chinekeboy <idokostephen93@gmail.com>
2025-12-02 15:11:47 +0100

    worked on 767 and 990px

--- script.js
+++ script.js
@@ -5,5 +5,11 @@ function goToReg() {
 // const paraElement = document.querySelector('.js-copy-button').classList('copy').textContent = new Date().getFullYear();
 
 
+function submit() {
+  const inputElement = document.querySelector('.first').value;
+  if(value === '') {
+    alert('input ur first name');
+  };
+};
 
 
